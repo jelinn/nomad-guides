a59aaeaf1fba3bee8d3acd0215b79e2cbc225fda
jelinn <jelinn@gmail.com>
2020-02-14 19:53:02 -0600

    Update app.tf

--- operations/provision-nomad/quick-start/terraform-aws/app.tf
+++ operations/provision-nomad/quick-start/terraform-aws/app.tf
@@ -34,7 +34,6 @@ resource "aws_security_group" "testenv_allow_all_DO_NOT_PROD" {
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = ["pl-12c4e678"]
   }
 }
 
