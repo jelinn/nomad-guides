700ab288ce920907fefeb530f0088412ae272c92
jelinn <jelinn@gmail.com>
2020-02-14 19:36:45 -0600

    Update app.tf

--- operations/provision-nomad/quick-start/terraform-aws/app.tf
+++ operations/provision-nomad/quick-start/terraform-aws/app.tf
@@ -14,11 +14,36 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_security_group" "testenv_allow_all_DO_NOT_PROD" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"] # add a CIDR block here
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = ["pl-12c4e678"]
+  }
+}
+
+
 resource "aws_instance" "client1" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
   key_name             = "jlinn-dev-useast2"
- 
+  vpc_security_group_ids = [aws_security_group.testenv_allow_all_DO_NOT_PROD]
   user_data = <<-EOT
     #! /bin/bash
     sudo apt-get update
@@ -32,10 +57,12 @@ resource "aws_instance" "client1" {
     Name = "jlinn-nomad-demo-consul-node"
   }
 }
+
 resource "aws_instance" "client2" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.nano"
   key_name             = "jlinn-dev-useast2"
+  vpc_security_group_ids = [aws_security_group.testenv_allow_all_DO_NOT_PROD]
 
   user_data = <<-EOT
     #! /bin/bash
